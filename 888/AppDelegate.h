9de97b641fb146e0abe0a835da857610edfeef12
qinong-tianyaoqi <1174465110@qq.com>
2018-04-02 14:40:05 +0800

    Initial Commit

---
+++ 888/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  888
+//
+//  Created by 田耀琦 on 2018/4/2.
+//  Copyright © 2018年 田耀琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 888/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  888
+//
+//  Created by 田耀琦 on 2018/4/2.
+//  Copyright © 2018年 田耀琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
